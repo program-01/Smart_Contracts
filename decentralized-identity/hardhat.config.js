14923e8b683e5831058d6860a1657b068dab441b
Cesar Lopez <cesarlopez@Cesars-MacBook-Pro.local>
2025-03-30 02:29:51 -0400

    initial work to environment and setting up etherium

---
+++ decentralized-identity/hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+};
+
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
